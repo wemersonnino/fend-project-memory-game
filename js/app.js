93f5c0e47f2066628eeef078c2039c92a781db44
wemerson <wemersonnino@gmail.com>
2018-07-21 13:56:11 -0300

    ouvinte click escolhe a carta

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+///* criando ouvinte click escolhe a carta.
+
+let clickTest = $('li');
+
+clickTest.click(function(){
+    $(this).toggleClass('match');
+   console.log('teste <li>'); 
+});
